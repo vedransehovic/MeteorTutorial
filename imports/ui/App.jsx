b629d88a94f134342b7fff5fde45a96a77fd9cc8
vedransehovic <vedran.sehovic@gmail.com>
2021-04-15 15:26:00 -0400

    initial changes to App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from "react";
+import { Hello } from "./Hello.jsx";
+import { Info } from "./Info.jsx";
+
+const tasks = [
+  { _id: 1, text: "First Task" },
+  { _id: 2, text: "Second Task" },
+  { _id: 3, text: "Third Task" },
+];
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>This will list tasks!</h1>
+    <Hello />
+    <Info />
   </div>
 );
