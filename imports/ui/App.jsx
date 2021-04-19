2646d345bc5307303b73b3e9662314cc3189e1aa
vedransehovic <vedran.sehovic@gmail.com>
2021-04-19 11:17:33 -0400

    daily commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import Task from "./Task";
+import { TaskForm } from "./TaskForm";
 
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
   return (
     <div>
-      <h1>This is a heading</h1>
-
+      <h1>Welcome to Meteor</h1>
+      <TaskForm />
       <ul>
         {tasks.map((task) => (
           <Task key={task._id} task={task} />
