247893640d6c90d8601fe4df47d81bceb1b4f1ed
vedransehovic <vedran.sehovic@gmail.com>
2021-04-20 11:19:23 -0400

    created Image Detail Component

---
+++ imports/ui/components/ImageList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Image Detail</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ImageList;
